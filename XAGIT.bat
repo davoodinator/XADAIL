97ba7d0a4a22bf3d3f6dda1ffbf5b0eb26fc292c
David Mohajer <david@xahive.com>
2016-06-24 02:16:05 -0400

    changelog and xagit cleanup for 0036
    0036 - ?
    engineer is already 1.2
    merged in 1.0.0.4HF2 changes
    Skelethon and Ranger are both called Coronus and Necromancer is Called
    Skelethon. - fixed
    fix coronus dead dogs skill
    removed unnecessary *tex files from \*\source\*
    updated git sync prep (XAGIT.bat) script to truly clone the work dir so
    i can delete stuff without double searching it
    *check component recipes are they borked?
    *cratebox still a problem. fixed - removed the clickable NPC item
    (cratebox) And placed the actual npc in the map in the appropriate
    location of the outcast hideout entrance. you can use him even if she is
    hostile.
    *add/test a pull script to put stuff back into my offline work dir when
    i merge something from the wild (if it ever happens)
    *add Lumera Relendra credit and npc
    *move the occultist and terrorknight component vendors
    *fix DGA token icons
    *fix hero soul icons
    *updated dawn of heroes to 1.4
    *check npc tags in DGA
    *update vanillabak folder with 1.0.0.4HF2 arz file
    *update engineer to 1.3

--- XAGIT.bat
+++ XAGIT.bat
@@ -1,19 +1,19 @@
 rem copy soft mods
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAIL\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAIL\/" /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAIL - RIFT\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAIL - RIFT\/" /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAIL - Survival\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAIL - Survival\/" /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAIL\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAIL\/" /MIR /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAIL - RIFT\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAIL - RIFT\/" /MIR /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAIL - Survival\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAIL - Survival\/" /MIR /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL
 
 rem copy hard mods
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - S\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - S\/" /S /XO /xf *.arz *.arc *.7z
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - A\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - A\/" /S /XO /xf *.arz *.arc *.7z
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - B\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - B\/" /S /XO /xf *.arz *.arc *.7z
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - AB\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - AB\/" /S /XO /xf *.arz *.arc *.7z
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - NG\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - NG\/" /S /XO /xf *.arz *.arc *.7z
-robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - A NG\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - A NG\/" /S /XO /xf *.arz *.arc *.7z
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - S\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - S\/" /MIR /S /XO /xf *.arz *.arc *.7z
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - A\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - A\/" /MIR /S /XO /xf *.arz *.arc *.7z
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - B\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - B\/" /MIR /S /XO /xf *.arz *.arc *.7z
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - AB\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - AB\/" /MIR /S /XO /xf *.arz *.arc *.7z
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - NG\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - NG\/" /MIR /S /XO /xf *.arz *.arc *.7z
+robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\DAILmain - A NG\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\DAILmain - A NG\/" /MIR /S /XO /xf *.arz *.arc *.7z
 
 rem copy hard mod backup for "uninstalling"
 rem removed the line because .arz files are >50 megs and github was complaining.. just in case the .arz files get even larger.....
-rem robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\VanillaBak\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\VanillaBak\/" /S /XO /xf *.arz *.arc
+rem robocopy /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\VanillaBak\/" /"\localhost\f\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\VanillaBak\/" /MIR /S /XO /xf *.arz *.arc
 copy "f:\SHTEEM\steamapps\common\Grim Dawn\mods\VanillaBak\VanillaBak.7z" "f:\SHTEEM\steamapps\common\Grim Dawn\mods\XADAIL\VanillaBak\" /y
 
 rem copy individual files
