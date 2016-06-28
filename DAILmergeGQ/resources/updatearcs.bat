074f847d5703ac25e0daff7432f0ca46496cf3bc
David Mohajer <david@xahive.com>
2016-06-28 01:38:56 -0400

    0039 - RELEASE - EXPERIMENTAL

--- DAILmergeGQ/resources/updatearcs.bat
+++ DAILmergeGQ/resources/updatearcs.bat
@@ -3,4 +3,5 @@ archivetool Effects.arc -update . Effects 6
 archivetool XPack.arc -update . XPack 6
 archivetool Items.arc -update . Items 6
 rem remmed out the pause. seems unnecessary
+rem add it back if you are having a problem!
 rem @pause
