a81056dfb4b86739c4f04c4729b11907973608fc
David Mohajer <david@xahive.com>
2016-07-14 19:53:28 -0400

    0049 prep
    changelog updates

--- DAILmain - S/resources/scripts/Omega_Mod/Omega_scripts.lua
+++ DAILmain - S/resources/scripts/Omega_Mod/Omega_scripts.lua
@@ -3,258 +3,29 @@
 
 omega.scripts = {}
 
---DAIL START
-dail = {}
-dail.scripts = {}
-dail.summon = {}
---DAIL END
-
 function omega.scripts.onherodeath()
-if Game.GetGameDifficulty() >= Game.Difficulty.Legendary then
-local player = Game.GetLocalPlayer();
-player:GiveItem('records/omega/items/questitems/Omega_Soul_Heroes.dbr', 1, true);
-	if 	player:GetQuestState(0x4B25F880) != QuestState.InProgress then
-		player:GrantQuest(0x4B25F880,0x36BE5A40);
-	end
-omega.scripts.spawnomegaboss();
-end
---DAIL START
-	math.randomseed(Time.Now());
-	local DAILpop = random(1, 100000);
-	--5% chance
-	if DAILpop < 5000 then
-		UI.Notify("DAIL_AETHERIAL_POP");
+	if Game.GetGameDifficulty() >= Game.Difficulty.Legendary then
 		local player = Game.GetLocalPlayer();
-		player:GiveItem('DAIL/items/summon/dailRNGpot.dbr', 1, true);
---		dail.scripts.spawndailitems();
---		dail.scripts.spawndailnpcs();
+		player:GiveItem('records/omega/items/questitems/Omega_Soul_Heroes.dbr', 1, true);
+			if 	player:GetQuestState(0x4B25F880) != QuestState.InProgress then
+				player:GrantQuest(0x4B25F880,0x36BE5A40);
+			end
+		omega.scripts.spawnomegaboss();
 	end
---DAIL END
+	callDAILonherodeath()
 end
 
-
---DAIL START
-function dail.scripts.spawndailitems()
-	local player = Game.GetLocalPlayer();
-		local dbrITEM = {
-		'DAIL/items/summon/dailskillsummon0001.dbr',
-		'DAIL/items/summon/dailskillsummon0002.dbr',
-		'DAIL/items/summon/dailskillsummon0003.dbr',
-		'DAIL/items/summon/dailskillsummon0004.dbr',
-		'DAIL/items/summon/dailskillsummon0005.dbr',
-		'DAIL/items/summon/dailskillsummon0006.dbr',
-		'DAIL/items/summon/dailskillsummon0007.dbr',
-		'DAIL/items/summon/dailskillsummon0008.dbr',
-		'DAIL/items/summon/dailskillsummon0009.dbr',
-		'DAIL/items/summon/dailskillsummon0010.dbr',
-		'DAIL/items/summon/dailskillsummon0011.dbr',
-		'DAIL/items/summon/dailskillsummon0012.dbr',
-		'DAIL/items/summon/dailskillsummon0013.dbr',
-		'DAIL/items/summon/dailskillsummon0016.dbr',
-		'DAIL/items/summon/dailskillsummon0017.dbr',
-		'DAIL/items/summon/dailskillsummon0018.dbr',
-		'DAIL/items/summon/dailskillsummon0019.dbr',
-		'DAIL/items/summon/dailskillsummon0020.dbr',
-		'DAIL/items/summon/dailskillsummon0021.dbr',
-		'DAIL/items/summon/dailskillsummon0022.dbr',
-		'DAIL/items/summon/dailskillsummon0023.dbr',
-		'DAIL/items/summon/dailskillsummon0024.dbr',
-		'DAIL/items/summon/dailskillsummon0026.dbr',
-		'DAIL/items/summon/dailskillsummon0027.dbr',
-		'DAIL/items/summon/dailskillsummon0028.dbr',
-		'DAIL/items/summon/dailskillsummon0029.dbr',
-		'DAIL/items/summon/cratebox.dbr',
-		'DAIL/items/summon/dailfactionsummon0001.dbr',
-		'DAIL/items/summon/dailfactionsummon0002.dbr',
-		'DAIL/items/summon/dailfactionsummon0003.dbr'
-	};
-	math.randomseed(Time.Now());
---	local randNPC = random(1, table.getn(dbrITEM));
-	local randITEM = random(1, 31);
---	local newNPC = Entity.Create(dbrNPC[randNPC]);
-	player:GiveItem(dbrITEM[randITEM], 1, true);
-
+function omega.scripts.onbossdeath()
+	if Game.GetGameDifficulty() >= Game.Difficulty.Legendary then
+		local player = Game.GetLocalPlayer();
+		player:GiveItem('records/omega/items/questitems/Omega_Soul_Boss.dbr', 1, true);
+	end
 end
-
---		'DAIL/creatures/npcs/merchants/DAILclassplusser0014.dbr',
---		'DAIL/creatures/npcs/merchants/DAILclassplusser0015.dbr',
---		'DAIL/creatures/npcs/merchants/DAILclassplusser0025.dbr',
---		'DAIL/creatures/npcs/merchants/DAILclassplusser0030.dbr',
-
-function dail.scripts.spawndailnpcs()
-	UI.Notify("DAIL_AETHERIAL_POP2");
-	local Player = Game.GetLocalPlayer();
-	local coords = Player:GetCoords();
-	local dbrNPC = {
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0001.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0002.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0003.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0004.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0005.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0006.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0007.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0008.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0009.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0010.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0011.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0012.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0013.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0016.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0017.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0018.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0019.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0020.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0021.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0022.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0023.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0024.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0026.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0027.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0028.dbr',
-		'DAIL/creatures/npcs/merchants/DAILclassplusser0029.dbr',
-		'records/creatures/npcs/merchants/transmogsmith.dbr',
-		'DAIL/creatures/npcs/npcs/davood.dbr',
-		'DAIL/creatures/npcs/npcs/khalanos.dbr',
-		'DAIL/creatures/npcs/npcs/hardy.dbr'
-	};
-	math.randomseed(Time.Now());
---	local randNPC = random(1, table.getn(dbrNPC));
-	local randNPC = random(1, 31);
-	local newNPC = Entity.Create(dbrNPC[randNPC]);
---	local playerCoords = Game.GetLocalPlayer():GetCoords();
---	newNPC:SetCoords(playerCoords);
-				if (newNPC != nil) then
-					newNPC:NetworkEnable()
-					newNPC:SetCoords(coords)
-				end
-end
-
-function dail.summon.item_cratebox()
- dail.summon.npc('records/creatures/npcs/merchant/transmogsmith.dbr')
- end
-function dail.summon.dailskillsummon0001()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0001.dbr')
- end
-function dail.summon.dailskillsummon0002()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0002.dbr')
- end
-function dail.summon.dailskillsummon0003()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0003.dbr')
- end
-function dail.summon.dailskillsummon0004()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0004.dbr')
- end
-function dail.summon.dailskillsummon0005()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0005.dbr')
- end
-function dail.summon.dailskillsummon0006()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0006.dbr')
- end
-function dail.summon.dailskillsummon0007()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0007.dbr')
- end
-function dail.summon.dailskillsummon0008()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0008.dbr')
- end
-function dail.summon.dailskillsummon0009()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0009.dbr')
- end
-function dail.summon.dailskillsummon0010()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0010.dbr')
- end
-function dail.summon.dailskillsummon0011()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0011.dbr')
- end
-function dail.summon.dailskillsummon0012()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0012.dbr')
- end
-function dail.summon.dailskillsummon0013()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0013.dbr')
- end
-function dail.summon.dailskillsummon0014()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0014.dbr')
- end
-function dail.summon.dailskillsummon0015()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0015.dbr')
- end
-function dail.summon.dailskillsummon0016()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0016.dbr')
- end
-function dail.summon.dailskillsummon0017()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0017.dbr')
- end
-function dail.summon.dailskillsummon0018()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0018.dbr')
- end
-function dail.summon.dailskillsummon0019()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0019.dbr')
- end
-function dail.summon.dailskillsummon0020()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0020.dbr')
- end
-function dail.summon.dailskillsummon0021()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0021.dbr')
- end
-function dail.summon.dailskillsummon0022()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0022.dbr')
- end
-function dail.summon.dailskillsummon0023()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0023.dbr')
- end
-function dail.summon.dailskillsummon0024()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0024.dbr')
- end
-function dail.summon.dailskillsummon0025()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0025.dbr')
- end
-function dail.summon.dailskillsummon0026()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0026.dbr')
- end
-function dail.summon.dailskillsummon0027()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0027.dbr')
- end
-function dail.summon.dailskillsummon0028()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0028.dbr')
- end
-function dail.summon.dailskillsummon0029()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0029.dbr')
- end
-function dail.summon.dailskillsummon0030()
- dail.summon.npc('DAIL/creatures/npcs/merchants/DAILclassplusser0030.dbr')
- end
-function dail.summon.dailfactionsummon00001()
- dail.summon.npc('DAIL/creatures/npcs/npcs/davood.dbr')
- end
-function dail.summon.dailfactionsummon00002()
- dail.summon.npc('DAIL/creatures/npcs/npcs/khalanos.dbr')
- end
-function dail.summon.dailfactionsummon00003()
- dail.summon.npc('DAIL/creatures/npcs/npcs/hardy.dbr')
- end
-
-function dail.summon.npc(dailNPC)
-	UI.Notify("DAIL_AETHERIAL_POP2");
-	local Player = Game.GetLocalPlayer();
-	local coords = Player:GetCoords();
-	local newNPC = Entity.Create(dailNPC);
-				if (newNPC != nil) then
-					newNPC:NetworkEnable()
-					newNPC:SetCoords(coords)
-				end
-end
-
---DAIL END
 
 function omega.scripts.summonatfeet(objectID)
-	local Player = Game.GetLocalPlayer();
-	local coords = Player:GetCoords();
-	--local namedbr ="records/creatures/enemies/hero/"..objectID..".dbr"
-	local test = Entity.Create(objectID);
-			
-				if (test != nil) then
-					test:NetworkEnable()
-					test:SetCoords(coords)
-				end
+	local newCreature = Entity.Create(objectID);
+	local playerCoords = Game.GetLocalPlayer():GetCoords();
+	newCreature:SetCoords(playerCoords);
 end
 
 function omega.scripts.spawnomegaboss()
@@ -276,12 +47,11 @@ local t={'OMEGA_LEVEL_1','OMEGA_LEVEL_2','OMEGA_LEVEL_3','OMEGA_LEVEL_4','OMEGA_
 	
 	if spOMbosschance > 10000 then
 		local spOMpetRandomizer = 0
-		spOMpetRandomizer = random(1,6)
-		if spOMpetRandomizer < 6 then
-			omega.scripts.summonatfeet('records/omega/creatures/om_spawnerpet.dbr');
-		else
-			omega.scripts.summonatfeet('records/omega/creatures/om_spawnerpet2.dbr');
-		end
+		local OmegaNemesisDBRs = { 'records/omega/creatures/om_spawnerpet_aetherial.dbr','records/omega/creatures/om_spawnerpet_chthonian.dbr','records/omega/creatures/om_spawnerpet_kymon.dbr','records/omega/creatures/om_spawnerpet_order.dbr','records/omega/creatures/om_spawnerpet_outlaw.dbr','records/omega/creatures/om_spawnerpet_undead.dbr' }
+		local totalOmegaNemesis = table.getn(OmegaNemesisDBRs)
+		spOMpetRandomizer = random(1,totalOmegaNemesis)
+		omega.scripts.summonatfeet(OmegaNemesisDBRs[spOMpetRandomizer]);
+
 	end
 end
 
@@ -342,7 +112,7 @@ local player = Game.GetLocalPlayer();
 		for i=1,100 do
 			if player:HasToken(t[i]) then
 				local tagnotify ='tagOM_notifylevel'..i
-				UI.Notify(tagnotify);				
+				UI.Notify(tagnotify);
 				break
 			end
 		end
@@ -360,4 +130,41 @@ function omega.scripts.bossMIonAddToWorld(objectId)
 					test:NetworkEnable()
 					test:SetCoords(itemCoords)
 				end
+end
+
+-----Give a low chance to summon the corresponding Omega boss when opening Nemesis chest in Ultimate Difficulty.
+function omega.scripts.onopenchestnemesis(objectID)
+	local player = Game.GetLocalPlayer()
+	if player:HasToken('OMEGA_START_QUEST') and Game.GetGameDifficulty() >= Game.Difficulty.Legendary then
+		local spOMbosschance = 0
+		math.randomseed(Time.Now())
+		spOMbosschance = random(1,100)
+			if spOMbosschance > 96 then
+					omega.scripts.summonatfeet(objectID)
+				else
+					if player:HasToken('OMEGA_BUFF_SPAWN') then
+						omega.scripts.summonatfeet(objectID)
+						player:RemoveToken('OMEGA_BUFF_SPAWN')
+					end
+			end
+	end
+end
+
+function omega.scripts.onopenchestaetherial()
+	omega.scripts.onopenchestnemesis('records/omega/creatures/om_spawnerpet_aetherial.dbr');
+end
+function omega.scripts.onopenchestchthonian()
+	omega.scripts.onopenchestnemesis('records/omega/creatures/om_spawnerpet_chthonian.dbr');
+end
+function omega.scripts.onopenchestkymon()
+	omega.scripts.onopenchestnemesis('records/omega/creatures/om_spawnerpet_kymon.dbr');
+end
+function omega.scripts.onopenchestorder()
+	omega.scripts.onopenchestnemesis('records/omega/creatures/om_spawnerpet_order.dbr');
+end
+function omega.scripts.onopenchestoutlaw()
+	omega.scripts.onopenchestnemesis('records/omega/creatures/om_spawnerpet_outlaw.dbr');
+end
+function omega.scripts.onopenchestundead()
+	omega.scripts.onopenchestnemesis('records/omega/creatures/om_spawnerpet_undead.dbr');
 end
