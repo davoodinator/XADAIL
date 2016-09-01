5534e22a42bf78af7547b4c54573f65f083b5401
kriescher1234 <klaus_home@gmx.de>
2016-09-01 17:11:48 +0200

    FormatException Fix
    FormatException Fix

--- MissingFilesSearch.exe.bat
+++ MissingFilesSearch.exe.bat
@@ -6,3 +6,4 @@ rem MissingFilesSearch.exe /path:"E:\Working\Grim Dawn\mods\XADAIL-1\DAIL\databa
 rem /path path to check
 rem /GrimDawnPath path to extracted GD extracted game files
 MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL\database" /GrimDawnPath:"E:\Working\Grim Dawn\database"
+MissingFilesSearch.exe /path:"%BATCH_DIR%DAIL - Survival\database" /GrimDawnPath:"E:\Working\Grim Dawn\database"
