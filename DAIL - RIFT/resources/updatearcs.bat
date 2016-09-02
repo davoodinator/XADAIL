8f68ce8459966617b63206fc4a42550e5c92a2d4
David Mohajer <david@xahive.com>
2016-09-02 01:20:23 -0400

    merged cftx doh 2016 09 02

---
+++ DAIL - RIFT/resources/updatearcs.bat
@@ -0,0 +1,8 @@
+"%pInstallDir%\archivetool" Effects.arc -update . Effects 6
+"%pInstallDir%\archivetool" XPack.arc -update . XPack 6
+"%pInstallDir%\archivetool" Items.arc -update . Items 6
+"%pInstallDir%\archivetool" Textures.arc -update . Textures 6
+"%pInstallDir%\archivetool" ui.arc -update . ui 6
+
+ECHO DAIL addition
+"%pInstallDir%\archivetool" Sound.arc -update . Items 6
