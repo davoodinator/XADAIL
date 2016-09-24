9e5df7486943ab902432bd8c48a705efb7b8d90d
David Mohajer <david@xahive.com>
2016-09-24 05:56:28 -0400

    fixnig the ui. again

--- XAGIT.bat
+++ XAGIT.bat
@@ -1,35 +1,35 @@
 call paths
 
 rem prep the root - mirror the root - ignore the subdirs when mirroring
-robocopy "%SOURCE_PATH%\" "%SOURCE_PATH%\XADAIL" /XO *.bat *.txt *.nsi README /MIR /xd *.* /xf cl*.txt
+robocopy "%SOURCE_PATH%\" "%SOURCE_PATH%\XADAIL"  *.bat *.txt *.nsi README /MIR /xd *.* /xf cl*.txt
 copy "%SOURCE_PATH%\DAIL\database\records\ui\skills\30_MasteriesClassSelection.7z" "%SOURCE_PATH%\XADAIL\DAIL\database\records\ui\skills" /y
 copy "%SOURCE_PATH%\DAIL\database\records\ui\skills\99_MasteriesClassSelection.7z" "%SOURCE_PATH%\XADAIL\DAIL\database\records\ui\skills" /y
-robocopy "%SOURCE_PATH%\_Dev_info" "%SOURCE_PATH%\XADAIL\_Dev_info" /XO /xd *.*
+robocopy "%SOURCE_PATH%\_Dev_info" "%SOURCE_PATH%\XADAIL\_Dev_info"  /xd *.*
 
 rem copy soft mods
-robocopy "%SOURCE_PATH%\DAIL" "%SOURCE_PATH%\XADAIL\DAIL" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL *.n *.bak /XD resources
-robocopy "%SOURCE_PATH%\DAIL - RIFT" "%SOURCE_PATH%\XADAIL\DAIL - RIFT" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL /XD resources
-robocopy "%SOURCE_PATH%\DAIL - Survival" "%SOURCE_PATH%\XADAIL\DAIL - Survival" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL /XD resources
-robocopy "%SOURCE_PATH%\DAIL - RIFT\assets" "%SOURCE_PATH%\XADAIL\DAIL - RIFT\assets" /NDL /NFL /S /XO /xf *.arz *.arc *.7z *.debug *.good *.DAIL /XD resources
-robocopy "%SOURCE_PATH%\DAIL - Survival\assets" "%SOURCE_PATH%\XADAIL\DAIL - Survival\assets" /NDL /NFL /S /XO /xf *.arz *.arc *.7z *.debug *.good *.DAIL /XD resources
+robocopy "%SOURCE_PATH%\DAIL" "%SOURCE_PATH%\XADAIL\DAIL" /NDL /NFL /MIR /S  /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL *.n *.bak /XD resources
+robocopy "%SOURCE_PATH%\DAIL - RIFT" "%SOURCE_PATH%\XADAIL\DAIL - RIFT" /NDL /NFL /MIR /S  /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL /XD resources
+robocopy "%SOURCE_PATH%\DAIL - Survival" "%SOURCE_PATH%\XADAIL\DAIL - Survival" /NDL /NFL /MIR /S  /xf *.arz *.arc *.map *.7z *.debug *.good *.DAIL /XD resources
+robocopy "%SOURCE_PATH%\DAIL - RIFT\assets" "%SOURCE_PATH%\XADAIL\DAIL - RIFT\assets" /NDL /NFL /S  /xf *.arz *.arc *.7z *.debug *.good *.DAIL /XD resources
+robocopy "%SOURCE_PATH%\DAIL - Survival\assets" "%SOURCE_PATH%\XADAIL\DAIL - Survival\assets" /NDL /NFL /S  /xf *.arz *.arc *.7z *.debug *.good *.DAIL /XD resources
 
 rem copy hard mods
-robocopy "%SOURCE_PATH%\DAILmain - S" "%SOURCE_PATH%\XADAIL\DAILmain - S" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z /XD resources
-robocopy "%SOURCE_PATH%\DAILmain - A" "%SOURCE_PATH%\XADAIL\DAILmain - A" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z /XD resources
-robocopy "%SOURCE_PATH%\DAILmain - B" "%SOURCE_PATH%\XADAIL\DAILmain - B" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z /XD resources
-rem robocopy "%SOURCE_PATH%\DAILmain - AB" "%SOURCE_PATH%\XADAIL\DAILmain - AB" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z
-rem robocopy "%SOURCE_PATH%\DAILmain - AX" "%SOURCE_PATH%\XADAIL\DAILmain - AX" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z
-robocopy "%SOURCE_PATH%\DAILmain - X" "%SOURCE_PATH%\XADAIL\DAILmain - X" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z /XD resources
-rem robocopy "%SOURCE_PATH%\DAILmain - SX" "%SOURCE_PATH%\XADAIL\DAILmain - SX" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z
-rem robocopy "%SOURCE_PATH%\DAILmain - ASX" "%SOURCE_PATH%\XADAIL\DAILmain - ASX" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z
-rem robocopy "%SOURCE_PATH%\DAILmain - A NG" "%SOURCE_PATH%\XADAIL\DAILmain - A NG" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z
+robocopy "%SOURCE_PATH%\DAILmain - S" "%SOURCE_PATH%\XADAIL\DAILmain - S" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z /XD resources
+robocopy "%SOURCE_PATH%\DAILmain - A" "%SOURCE_PATH%\XADAIL\DAILmain - A" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z /XD resources
+robocopy "%SOURCE_PATH%\DAILmain - B" "%SOURCE_PATH%\XADAIL\DAILmain - B" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z /XD resources
+rem robocopy "%SOURCE_PATH%\DAILmain - AB" "%SOURCE_PATH%\XADAIL\DAILmain - AB" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z
+rem robocopy "%SOURCE_PATH%\DAILmain - AX" "%SOURCE_PATH%\XADAIL\DAILmain - AX" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z
+robocopy "%SOURCE_PATH%\DAILmain - X" "%SOURCE_PATH%\XADAIL\DAILmain - X" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z /XD resources
+rem robocopy "%SOURCE_PATH%\DAILmain - SX" "%SOURCE_PATH%\XADAIL\DAILmain - SX" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z
+rem robocopy "%SOURCE_PATH%\DAILmain - ASX" "%SOURCE_PATH%\XADAIL\DAILmain - ASX" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z
+rem robocopy "%SOURCE_PATH%\DAILmain - A NG" "%SOURCE_PATH%\XADAIL\DAILmain - A NG" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z
 
 rem copy hard mod backup for "uninstalling"
 rem removed the line because .arz files are >50 megs and github was complaining.. just in case the .arz files get even larger.....
-rem robocopy "%SOURCE_PATH%\VanillaBak" "%SOURCE_PATH%\XADAIL\VanillaBak" /NDL /NFL /MIR /S /XO /xf *.arz *.arc
+rem robocopy "%SOURCE_PATH%\VanillaBak" "%SOURCE_PATH%\XADAIL\VanillaBak" /NDL /NFL /MIR /S  /xf *.arz *.arc
 copy "%SOURCE_PATH%\VanillaBak\VanillaBak.7z" "%SOURCE_PATH%\XADAIL\VanillaBak" /y
 
 rem copy the Grim Quest and TEXT_EN merger files
-robocopy "%SOURCE_PATH%\DAILmergeGQ" "%SOURCE_PATH%\XADAIL\DAILmergeGQ" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z
-robocopy "%SOURCE_PATH%\dailFICULTIES" "%SOURCE_PATH%\XADAIL\dailFICULTIES" /NDL /NFL /MIR /S /XO /xf *.arz *.arc *.7z
-robocopy "%SOURCE_PATH%\survivalmode" "%SOURCE_PATH%\XADAIL\survivalmode" /NDL /NFL /MIR /S /XO *.bat
+robocopy "%SOURCE_PATH%\DAILmergeGQ" "%SOURCE_PATH%\XADAIL\DAILmergeGQ" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z
+robocopy "%SOURCE_PATH%\dailFICULTIES" "%SOURCE_PATH%\XADAIL\dailFICULTIES" /NDL /NFL /MIR /S  /xf *.arz *.arc *.7z
+robocopy "%SOURCE_PATH%\survivalmode" "%SOURCE_PATH%\XADAIL\survivalmode" /NDL /NFL /MIR /S  *.bat
