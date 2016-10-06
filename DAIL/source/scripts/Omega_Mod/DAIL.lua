6f323fd00cd08614d3629f355f37196149101bb9
Sean Becker <kathanious@gmail.com>
2016-10-05 22:19:14 -0500

    Revert "Survival Update"
    This reverts commit b802b99493d7a1c9ffbe008d96d244e087b7e204.

--- DAIL/source/scripts/Omega_Mod/DAIL.lua
+++ DAIL/source/scripts/Omega_Mod/DAIL.lua
@@ -1,11 +1,20 @@
 ----THIS FILE is FOR DAIL MOD and will be erased/replaced by DAIL scripts.It does nothing for OMEGA MOD
----The function callDAILonherodeath is the only one called by omega scipts on hero death on any difficulty.
+---The function callDAILonherodeath is the only one called by omega scripts on hero death on any difficulty.
 --DAIL START
 dail = {}
-dail.scripts = {}
-dail.summon = {}
+-- dail.scripts = {}
+-- dail.summon = {}
+--DAIL END
+
 --DAIL START
 function callDAILonherodeath()
-		--UI.Notify("DAIL_MOD_CALLED");
+	--math.randomseed(Time.Now());
+
+	--local DAILpop = math.random(1, 100000);
+	
+	-- if DAILpop < 35000 then
+		UI.Notify("DAIL_AETHERIAL_POP");
+		local player = Game.GetLocalPlayer();
+		player:GiveItem('DAIL/items/summon/dailRNGpot.dbr', 1, true);
+	-- end
 end
---DAIL END
